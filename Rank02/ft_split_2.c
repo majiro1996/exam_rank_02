36eb9bc172e79e2161909639cdfbe9d118207a08
Majiro <100861756+majiro1996@users.noreply.github.com>
2023-03-15 19:08:35 +0100

    Add files via upload

---
+++ Rank02/ft_split_2.c
@@ -0,0 +1,79 @@
+
+#include <stdlib.h>
+
+int is_space(char a)
+{
+	if (a == ' ' || a == '\t' || a == '\n')
+		return(1);
+	return(0);
+}
+
+char *fill(char *s)
+{
+	char *result;
+	static int c;
+	int len = 0;
+	int k = 0;
+	while(is_space(s[c]))
+		c++;
+	while(s[c] && !is_space(s[c]))
+	{
+		len++;
+		c++;
+	}
+	result = malloc(sizeof(char) * len + 1);
+	if(!result)
+		return (NULL);
+	result[len] = '\0';
+	c = c -len;
+	while(k < len)
+	{
+		result[k] = s[c];
+		k++;
+		c++;
+	}
+	c++;
+	return (result);
+}
+
+char    **ft_split(char *str)
+{
+	int wc = 0;
+	int c = 0;
+	char **m;
+	int current = 0;
+	while(str[c])
+	{
+		if(!is_space(str[c]) && (is_space(str[c + 1]) || !str[c + 1]))
+			wc++;
+		c++;
+	}
+	m = malloc(sizeof(char *) * wc + 1);
+	if(!m)
+		return(NULL);
+	m[wc + 1]=NULL;
+	while(current <= wc)
+	{
+		m[current] = fill(str);
+		current++;
+	}
+	return (m);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	int		idx;
+	char	**tab;
+
+	idx = 0;
+	tab = ft_split("   hola    holita   vecinito    ");
+	while (idx < 3)
+	{
+		printf("String %d : %s\n", idx, tab[idx]);
+		idx++;
+	}
+	return (0);
+}
+
